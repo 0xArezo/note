84ff91bcf086e7e2a3546a3502eccf13fbb58ebe
0xArezo <140800360+0xArezo@users.noreply.github.com>
2024-12-10 06:12:29 +0100

    Update directory.cpp

--- directory.cpp
+++ directory.cpp
@@ -3,7 +3,7 @@
 
 namespace fs = std::filesystem;
 
-// تابع بازگشتی برای نمایش دایرکتوری‌ها به صورت درختی
+// تابع بازگشتی برای نمایش دایرکتوری‌ها به صورت درختی زیبا
 void print_directory_tree(const fs::path& path, const std::string& prefix = "") {
     // بررسی وجود مسیر و اطمینان از اینکه مسیر دایرکتوری است
     if (fs::exists(path) && fs::is_directory(path)) {
