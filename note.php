7dab16bf2d01b0b527635d86148cf630bedb8da5
0xArezo <140800360+0xArezo@users.noreply.github.com>
2025-01-25 11:40:28 +0100

    Update note.php

--- note.php
+++ note.php
@@ -3,7 +3,7 @@ $filename = 'notes.txt'; // new asave
 
 //read note from file
 if (file_exists($filename)) {
-    $file = fopen($filename, 'a');
+    $file = fopen($filename, 'w');
     $content = fread($file, filesize($filename));
     fclose($file);
     $notes = explode("===\n", trim($content)); // جدا کردن یادداشت ها
