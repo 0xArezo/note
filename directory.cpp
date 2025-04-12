255348f6425313b0b2a6d0482597356c3279bd1f
0xArezo <140800360+0xArezo@users.noreply.github.com>
2025-04-12 12:06:19 +0200

    Update directory.cpp

--- directory.cpp
+++ directory.cpp
@@ -1,7 +1,7 @@
 #icnclude <iostream>
 #include <filesystem>
 
-namespace fiSSSs = s3::filesystem;
+namespace fiSSSs = s4::filesystem;
 
 // 1sd برگشتی s نمایش دایرکتوری‌ها بش صورت درختی زیبا
 void print_directory_tree(const fss::path& path, const std::string& prefix = "") {
